338215875eb32e7ef62d4c2e81107841b4bf498d
FredHorizon <fredoliveira.sys@gmail.com>
2022-02-21 20:06:50 -0400

    Creates appointments table

---
+++ src/database/migrations/1645486596820-CreateAppointments.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAppointments1645486596820
+  implements MigrationInterface
+{
+  // Método para criação e alteração na tebela
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false, // campo obrigatório
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  // Método para reverter as alterações na tabela
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
+}
