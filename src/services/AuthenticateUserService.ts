5729a3e98e8b34986aa1cc645bd45e734a835c2c
FredHorizon <fredoliveira.sys@gmail.com>
2022-03-02 16:55:42 -0400

    Configures token generation

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
 import User from '../models/User';
 
 interface Request {
@@ -9,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -29,10 +32,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination');
     }
 
-    // Usuário autenticado
+    const token = sign({}, '1807d1c0ca39725b795a7c9ad892d972', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
 
     return {
       user,
+      token,
     };
   }
 }
