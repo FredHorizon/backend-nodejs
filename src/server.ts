d6ab2083539dbef215fa16127182c6bfe0cf301b
FredHorizon <fredoliveira.sys@gmail.com>
2022-03-04 12:29:57 -0400

    Serves static files

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,13 @@ import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
 
+import uploadConfig from './config/upload';
 import './database';
 
 const app = express();
 
 app.use(express.json());
-
+app.use('/files', express.static(uploadConfig.directory)); // servir arquivos de forma estática
 app.use(routes); // routes torna-se um middleware agora
 
 app.listen(3333, () => {
